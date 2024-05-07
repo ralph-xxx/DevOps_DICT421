5442d5e542c42b41ba3fab39525ac2e7cd884989
Ralph <rmj.ralph@gmail.com>
2024-05-07 15:10:12 +0200

    new commit for test

---
+++ src/main/java/Makofane201876493.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class Makofane201876493 {
+    public static void main(String[] args) {
+        int num1 = 6;
+        int num2 = 6;
+        
+        int sum = num1 + num2;
+        
+        System.out.println("Addition of " + num1 + " and " + num2 + " gives" + sum);
+    }
+    
+}
